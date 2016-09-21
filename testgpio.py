ed8475bc564a95949bda209325759e9640aa58e1
henrisilver <henrisilver@hotmail.com>
2016-09-21 01:34:41 -0300

    Added small scripts to experiment with GPIO ports and Flask

---
+++ testgpio.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+
+def main():
+    print("Starting LED-io")
+
+    GPIO.setmode(GPIO.BCM)
+
+    GPIO.setup(17, GPIO.OUT)
+
+    print("\nEnter an option:\n1 - Turn on LED\n2 - Turn off LED\n0 - Quit\nOption: ")
+
+    option = input()
+
+    while option != 0:
+        # print("\n\nOption: ")
+        # print(option)
+        # print("\n\n")
+        if option == 1:
+            GPIO.output(17, True)
+        elif option == 2:
+            GPIO.output(17, False)
+        option = input("Enter option again: ")
+
+    GPIO.cleanup()
+
+if __name__=="__main__":
+    try:   
+        main()
+    except:
+	GPIO.cleanup()
