2c8538136670e9cd1a61dab0132d6ff1c7e3c25a
Henrique Silveira <hd2312@columbia.edu>
2016-10-19 19:41:33 -0200

    Implementing sensor logic in flask app

--- appflask.py
+++ appflask.py
@@ -1,24 +1,48 @@
 from flask import Flask
+from gpiozero import MotionSensor
 import RPi.GPIO as GPIO
+import time
+import datetime
+import threading
 
 app = Flask(__name__)
 status = False
+presenceTimeStamps = []
 
-@app.route("/on/")
+def motionSensor():
+    sensor = MotionSensor(4)
+    global presenceTimeStamps
+    global lock
+    while True:
+    if sensor.motion_detected:
+        currentTime = time.time()
+        timestamp = datetime.datetime.fromtimestamp(currentTime).strftime('%Y-%m-%d %H:%M:%S') 
+        presenceTimeStamps.append(timestamp)
+        time.sleep(2)
+
+@app.route("presence")
+def presence():
+    global presenceTimeStamps
+    stamps = ""
+    for ts in presenceTimeStamps:
+        stamps = stamps + str(ts)
+    return stamps
+
+@app.route("light/on/")
 def on():
     GPIO.output(17, True)
     global status
     status = True
     return "LED on"
 
-@app.route("/off/")
+@app.route("light/off/")
 def off():
     GPIO.output(17, False)
     global status
     status = False
     return "LED off"
 
-@app.route("/status/")
+@app.route("light/status/")
 def status():
     global status
     if status:
@@ -30,6 +54,7 @@ def status():
 if __name__ == "__main__":
     try:
         status = False
+        t = threading.Thread(target=motionSensor)
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(17, GPIO.OUT)
         app.run(host="189.5.253.103", port=5000, debug=True)
